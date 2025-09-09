c50000e91f8df06be5c1de3586721d6d92010950
Fleserig <fleserig@waspbyte.com>
2025-09-09 23:43:27 +0200

    Change layout End screen

--- app/src/main/java/com/waspbyte/piapp/EndScreenActivity.kt
+++ app/src/main/java/com/waspbyte/piapp/EndScreenActivity.kt
@@ -1,7 +1,9 @@
 package com.waspbyte.piapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
@@ -14,32 +16,45 @@ class EndScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end_screen)
 
-        val scoreTv = findViewById<TextView>(R.id.score_tv)
+        val accuracyTv = findViewById<TextView>(R.id.accuracy_tv)
+        val indexTv = findViewById<TextView>(R.id.index_tv)
+        val streakTv = findViewById<TextView>(R.id.streak_tv)
         val score = intent.getFloatExtra("SCORE", 1.0f)
         val currentIndex = intent.getIntExtra("CURRENT_INDEX", 0)
 
-        scoreTv.text = "Your score: ${"%.2f".format(score * 100f)}%"
+        indexTv.text = "Reached: $currentIndex"
+        accuracyTv.text = "Accuracy: ${"%.2f".format(score * 100f)}%"
+
         val sharedPref = getSharedPreferences(getString(R.string.prefs), MODE_PRIVATE)
         val previousIndex = sharedPref.getInt(getString(R.string.index), 0)
+
+        val today = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
+        val isNewDay = sharedPref.getString(getString(R.string.previous_date), today) != today || sharedPref.getInt(getString(R.string.streak), 0) == 0
         if (score >= 0.9 && previousIndex < currentIndex) {
             with (sharedPref.edit()) {
                 putInt(getString(R.string.index), currentIndex)
                 apply()
             }
-            val today = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
-            if (sharedPref.getString(getString(R.string.previous_date), today) != today) {
+            if (isNewDay) {
                 with(sharedPref.edit()) {
                     putString(getString(R.string.previous_date), today)
                     putInt(getString(R.string.streak), sharedPref.getInt(getString(R.string.streak), 0) + 1)
                     apply()
                 }
-            } else if (sharedPref.getInt(getString(R.string.streak), 0) == 0) {
-                with(sharedPref.edit()) {
-                    putString(getString(R.string.previous_date), today)
-                    putInt(getString(R.string.streak), 1)
-                    apply()
-                }
+
+                streakTv.text = "Reached daily goal!"
             }
+        } else if (isNewDay) {
+            streakTv.text = "Failed to reach daily goal."
+        }
+
+        findViewById<Button>(R.id.back_btn).setOnClickListener {
+            finish()
+        }
+
+        findViewById<Button>(R.id.again_btn).setOnClickListener {
+            startActivity(Intent(this, GameActivity::class.java))
+            finish()
         }
     }
 }
